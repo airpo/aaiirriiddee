6951c5b11bb4f6a86f4e3aa3158f319ee226e429
aaaaaaaa <a@a.jp>
2017-01-04 20:16:25 +0900

    create proj

---
+++ Source/AirRide/AirRideGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "AirRideGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AIRRIDE_API AAirRideGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
